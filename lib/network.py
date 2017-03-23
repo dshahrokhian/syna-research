a25d2be53f8186fde97b37c283c831002df90844
Daniyal Shahrokhian <dani.shahrokhian@gmail.com>
2017-03-23 15:41:16 +0100

    Remove unecessary pre-processing steps. Change performance plot.

--- lib/network.py
+++ lib/network.py
@@ -12,11 +12,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 import math
+from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from keras.utils.np_utils import to_categorical
 
 # Data Loader imports
 from dataloader.openface_dataloader import load_AU_activations
@@ -85,7 +87,7 @@ def main():
     openface_dir = os.path.join(os.getcwd(), "datasets/ck+parsed")
     ck_dir = os.path.join(os.getcwd(), "datasets/ck+")
     features, labels = load_data(openface_dir, ck_dir)
-
+    labels = to_categorical(labels)
     # normalize the dataset
     scaler = MinMaxScaler(feature_range=(0, 1))
     #dataset = scaler.fit_transform(dataset)
@@ -93,72 +95,28 @@ def main():
     # split into train and test sets
     train_size = int(len(features) * 0.67)
     test_size = len(features) - train_size
-    train_features, test_features = features[0:train_size], features[train_size:len(features)]
-    train_labels, test_labels = labels[0:train_size], labels[train_size:len(labels)]
-    print(train_features)
-    # convert an array of values into a dataset matrix
-    def create_dataset(features, labels, look_back=1):
-        dataX, dataY = [], []
-        print(features)
-        print("yo")
-        for i in range (len(features)):
-            a = []
-            for j in range(len(features[i])-look_back-1):
-                b = features[i][j:(j+look_back)]
-                a.append(b)
-            dataX.append(a)
-            dataY.append(labels[i])
-        print(dataX)
-        return np.array(dataX), np.array(dataY)
+    x_train, x_test = np.array(features[0:train_size]), np.array(features[train_size:len(features)])
+    y_train, y_test = np.array(labels[0:train_size]), np.array(labels[train_size:len(labels)])
     
-    # reshape into X=t and Y=t+1
     look_back = 1
-    trainX, trainY = create_dataset(train_features, train_labels, look_back)
-    testX, testY = create_dataset(test_features, test_labels,  look_back)
+    maxlen = 71 # Maximum frames of a record from the Cohn-Kanade dataset
+
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+    
     print("created datasets: ")
-    print(trainX, trainY)
-    
-    # reshape input to be [samples, time steps, features]
-    trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], look_back, trainX.shape[3]))
-    testX = np.reshape(testX, (testX.shape[0], testX.shape[1], look_back, testX.shape[3]))
-    print("reshaped datasets: ")
-    print(trainX[0])
-    
+    print(x_train[0])
     # create and fit the LSTM network
     model = Sequential()
     
-    model.add(LSTM(4, input_shape=(trainX.shape[1],look_back,18)))
-    model.add(Dense(1))
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
-    model.fit(trainX, trainY, nb_epoch=100, batch_size=1, verbose=2)
+    model.add(LSTM(4, input_shape=(x_train.shape[1],x_train.shape[2])))
+    model.add(Dense(8))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), nb_epoch=300, batch_size=1, verbose=2)
     
-    # make predictions
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
-    # invert predictions
-    trainPredict = scaler.inverse_transform(trainPredict)
-    trainY = scaler.inverse_transform([trainY])
-    testPredict = scaler.inverse_transform(testPredict)
-    testY = scaler.inverse_transform([testY])
-    # calculate root mean squared error
-    trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-    print('Train Score: %.2f RMSE' % (trainScore))
-    testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-    print('Test Score: %.2f RMSE' % (testScore))
-    
-    # shift train predictions for plotting
-    trainPredictPlot = np.empty_like(dataset)
-    trainPredictPlot[:, :] = np.nan
-    trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-    # shift test predictions for plotting
-    testPredictPlot = np.empty_like(dataset)
-    testPredictPlot[:, :] = np.nan
-    testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-    # plot baseline and predictions
-    plt.plot(scaler.inverse_transform(dataset))
-    plt.plot(trainPredictPlot)
-    plt.plot(testPredictPlot)
-    plt.show()
+    # Final evaluation of the model
+    scores = model.evaluate(x_test, y_test, verbose=0)
+    print("Accuracy: %.2f%%" % (scores[1]*100))
 
 if __name__ == "__main__":
     main()
