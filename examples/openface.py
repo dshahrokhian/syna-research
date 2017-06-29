Add normalization for irregularly-shaped lists. Fix code bug (missing comma)

examples/openface.py
@@ -8,6 +8,8 @@ __author__ = "Daniyal Shahrokhian <daniyal@kth.se>"
 
 # Neural Network imports
 import os
+import datetime
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
@@ -23,6 +25,7 @@ from bayes_opt import BayesianOptimization
 from deepmotion.dataloader.openface_dataloader import load_OpenFace_features
 from deepmotion.dataloader.ck_dataloader import load_CK_emotions
 from deepmotion.dataloader.afew_dataloader import load_AFEW_emotions
+import train_utils
 
 features, labels = None, None
 
@@ -61,7 +64,7 @@ def dicts2lists(dict_features, dict_emotions):
     return np.array(l_features), np.array(l_emotions)
 
 
-def load_ck_data(openface_dir, emotion_dir, data_type='AUs'):
+def load_ck_data(openface_dir, emotion_dir, feature_type='AUs'):
     """ 
     Extracts OpenFace Action Units features and CK+ Emotion labels,
     preserving the order (e.g. x_train[0] corresponds to the same sample as
@@ -71,19 +74,19 @@ def load_ck_data(openface_dir, emotion_dir, data_type='AUs'):
     ----------
     openface_dir : root directory of the parsed dataset with OpenFace
     emotion_dir : root directory of the CK+ dataset
-    data_type : which features to load {AUs, AU_activations, 2Dlandmarks}
+    feature_type : which features to load {AUs, AU_activations, 2Dlandmarks}
     
     Returns
     -------
     List, List
         OpenFace features, CK+ emotion labels
     """
-    features = load_OpenFace_features(openface_dir, features=data_type)
+    features = load_OpenFace_features(openface_dir, features=feature_type)
     labels = load_CK_emotions(emotion_dir)
 
     return dicts2lists(features, labels)
 
-def load_afew_data(openface_dir, emotion_dir, data_type='AUs'):
+def load_afew_data(openface_dir, emotion_dir, feature_type='AUs'):
     """ 
     Extracts OpenFace Action Units features and AFEW Emotion labels,
     preserving the order (e.g. x_train[0] corresponds to the same sample as
@@ -93,7 +96,7 @@ def load_afew_data(openface_dir, emotion_dir, data_type='AUs'):
     ----------
     openface_dir : root directory of the parsed dataset with OpenFace
     emotion_dir : root directory of the AFEW dataset
-    data_type : which features to load {AUs, AU_activations, 2Dlandmarks}
+    feature_type : which features to load {AUs, AU_activations, 2Dlandmarks}
     
     Returns
     -------
@@ -101,15 +104,13 @@ def load_afew_data(openface_dir, emotion_dir, data_type='AUs'):
         OpenFace train features, OpenFace test features, 
         AFEW train emotion labels, AFEW test emotion labels
     """
-    train_action_units = load_OpenFace_features( os.path.join(openface_dir, 'Train'), features=data_type )
+    train_action_units = load_OpenFace_features( os.path.join(openface_dir, 'Train'), features=feature_type )
     train_emotions = load_AFEW_emotions(emotion_dir, set='Train')
     train_features, train_labels = dicts2lists(train_action_units, train_emotions)
-    train_labels = to_categorical(train_labels)
 
-    test_action_units = load_OpenFace_features( os.path.join(openface_dir, 'Val'), features=data_type )
+    test_action_units = load_OpenFace_features( os.path.join(openface_dir, 'Val'), features=feature_type )
     test_emotions = load_AFEW_emotions(emotion_dir, set='Val')
     test_features, test_labels = dicts2lists(test_action_units, test_emotions)
-    test_labels = to_categorical(test_labels)
 
     x_train, x_test = np.array(train_features), np.array(test_features)
     y_train, y_test = np.array(train_labels), np.array(test_labels)
@@ -117,6 +118,8 @@ def load_afew_data(openface_dir, emotion_dir, data_type='AUs'):
     return x_train, x_test, y_train, y_test
 
 def adam_evaluate(neurons, lr, lr_decay, epochs, batch_size):
+    print("New evaluation")
+    print(datetime.datetime.now())
     # The Gaussian Process' space is continous, so we need to round these values
     neurons, epochs, batch_size = map(lambda x: int(round(x)), (neurons, epochs, batch_size))
 
@@ -129,8 +132,8 @@ def adam_evaluate(neurons, lr, lr_decay, epochs, batch_size):
         y_train, y_test = to_categorical(labels[train_index]), to_categorical(labels[test_index]) 
 
         # Create and fit the LSTM network
-        model = deepmotion.get_model(layers=[neurons], lr=lr, lr_decay=lr_decay input_shape=(None,len(x_train[0][0])))
-        #model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size, verbose=2)
+        model = deepmotion.get_model(layers=[neurons], lr=lr, lr_decay=lr_decay, input_shape=(None,len(x_train[0][0])))
+
         for _ in range(epochs):
             for X, Y in zip(x_train, y_train):
                 model.train_on_batch(np.array([X]), np.array([Y]))
@@ -139,13 +142,13 @@ def adam_evaluate(neurons, lr, lr_decay, epochs, batch_size):
         accuracies = []
         for X, Y in zip(x_test, y_test):
             accuracies.append(model.test_on_batch(np.array([X]), np.array([Y]))[1])
+        
         acc = np.mean(accuracies)
-        #print("Accuracy: %.2f%%" % (acc*100))
         scores.append(acc)
+        
     print("Accuracy and Standard dev: %.2f%% (+/- %.2f%%)" % (np.mean(scores), np.std(scores)))
-        #scores = model.evaluate(x_test, y_test, verbose=0)
-        #print("Accuracy: %.2f%%" % (scores[1]*100))
-
+    print(datetime.datetime.now())
+    
     return np.mean(scores)
 
 def main():
@@ -153,14 +156,15 @@ def main():
     features_dir = os.path.join(os.path.dirname(__file__), "..", "datasets/ck+norm")
     labels_dir = os.path.join(os.path.dirname(__file__), "..", "datasets/ck+")
     
-    for data_type in ['AUs', 'AU_activations', '2Dlandmarks']:
-        print("Using " + data_type)
+    for feature_type in ['AU_activations', 'AUs', '2Dlandmarks']:
+        print("Using " + feature_type)
+        
         # Fix random seed for reproducibility
         np.random.seed(7)
         
         global features, labels
-        features, labels = load_ck_data(features_dir, labels_dir, data_type=data_type)
-        features = preprocessing.scale(features) # Gaussian Normalization
+        features, labels = load_ck_data(features_dir, labels_dir, feature_type=feature_type)
+        features = train_utils.normalize(features) # Input normalization
 
         # Normalize length with zero-padding
         #maxlen = 71 # Maximum frames of a record from the Cohn-Kanade dataset
@@ -174,9 +178,13 @@ def main():
                                                     'lr_decay': (0.0, 1e-4),
                                                     'batch_size': (1, 1)
                                                     })
+        print("start")
+        print(datetime.datetime.now())
         hyper_opt.maximize()
+        print("finish")
+        print(datetime.datetime.now())
         
-        print("Best settings: " + str(hyper_opt.res['max']))        
+        print("Best hyperparameter settings: " + str(hyper_opt.res['max']))        
                     
 if __name__ == "__main__":
-    main()  
+    main()